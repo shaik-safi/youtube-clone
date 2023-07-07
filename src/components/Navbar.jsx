a3bbb73676b485c2411c76f905b567dce92c5b90
SHAIK SAFI <71938655+shaik-safi@users.noreply.github.com>
2023-07-07 20:38:59 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,13 @@ import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-        <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+  <Stack direction="row" alignItems="center" p={2} sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
       <img src={logo} alt="logo" height={45} />
-      <Typography variant="h4" fontWeight="bold" height={45} m={1} sx={{color: "white", fontFamily: "Arial, sans-serif", letterSpacing: "-0.04em" }}>
+      <Typography variant="h4" fontWeight="bold" height={45} m={1} sx={{ color: 'white', fontFamily: 'Arial, sans-serif', letterSpacing: '-0.04em', '@media (max-width: 700px)': { display: 'none' } }}>
         YouTube
       </Typography>
     </Link>
-    
     <SearchBar />
   </Stack>
 );
